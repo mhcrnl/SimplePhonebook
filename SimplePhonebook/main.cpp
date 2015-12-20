6f2c321546a56f69598ebfc6f0396dcce6191855
Suabahasa <suabahasa@gmail.com>
2015-12-20 21:18:53 +0700

    phoneBook struct

--- SimplePhonebook/main.cpp
+++ SimplePhonebook/main.cpp
@@ -4,25 +4,37 @@
 	(c) 2015 Nusantara Project.
 */
 
-#define _CRT_SECURE_NO_WARNINGS //Error	C4996	'scanf': This function or variable may be unsafe.Consider using scanf_s instead.To disable deprecation, use _CRT_SECURE_NO_WARNINGS.See online help for details.SimplePhonebook	e : \binus\kuliah\semester 1\alghoritm and programming\assigment\project\simplephonebook\simplephonebook\main.cpp
+#define _CRT_SECURE_NO_WARNINGS //Error	C4996	'scanf': This function or variable may be unsafe.Consider using scanf_s instead.To disable deprecation, use _CRT_SECURE_NO_WARNINGS.See online help for details.
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<ctype.h>
 
-/* Function prototyping */
-int newContact();
-int listContact();
-int searchContact();
-int deleteContact();
-int storeContact();
-
 char bilas; // fflush variable
+
+/* phoneBook structure */
+struct phoneBook {
+	int index;
+	char name[64],
+		phoneNumber[64];
+};
+
+
+/* Function prototyping */
+int newContact(int, struct phoneBook*);
+int listContact(int, struct phoneBook*);
+int searchContact(int, struct phoneBook*);
+int deleteContact(int, struct phoneBook*);
+int sortContact(int, struct phoneBook*);
+int storeContact(int, struct phoneBook*);
 
 int main()
 {
-	int menu;
+	int menu,
+		counter = 0; // contact counter
+	struct phoneBook contact[200]; // The old nokia phone model. If you know what I mean. haha
+	
 
 	/* Menu selection */
 	do
@@ -41,33 +53,38 @@ int main()
 	
 		switch (menu) {
 		case 1:
-			newContact();
+			newContact(counter, contact);
+			counter++;
+			sortContact(counter, contact);
 			break;
 		case 2:
-			listContact();
+			listContact(counter, contact);
 			break;
 		case 3:
-			searchContact();
+			searchContact(counter, contact);
 			break;
 		case 4:
-			deleteContact();
+			deleteContact(counter, contact);
+			sortContact(counter, contact);
+			counter--;
 			break;
 		case 5:
-			storeContact();
+			storeContact(counter, contact);
 			break;
 		}
 	} while (menu != 5);
+
 
 	return 0;
 }
 
 /* Add new contact to %Structure_Variables */
-int newContact()
+int newContact(int counter, struct phoneBook *contact)
 {
 	char name[64],
 		phoneNumber[64];
 	int i,
-		validate = 0;
+		validate;
 
 	system("cls");
 	printf("Add New Contact\n");
@@ -84,7 +101,7 @@ int newContact()
 			validate += 1;
 		}
 
-		if (isalpha(name[0]) == 0)
+		if ((name[0] < -1 || name[0] > 255) || isalpha(name[0]) == 0) //Debug Assertion Failed
 		{
 			printf("\tFirst letter of name should be an alphabet\n");
 			validate += 1;
@@ -106,7 +123,7 @@ int newContact()
 
 		for (i = 0; i < strlen(phoneNumber); i++)
 		{
-			if (isalpha(phoneNumber[i]) != 0)
+			if ((name[0] < -1 || name[0] > 255) || isalpha(phoneNumber[i]) != 0) //Debug Assertion Failed
 			{
 				printf("\tPhone numbers should only contain digits 0-9\n");
 				validate += 1;
@@ -116,21 +133,38 @@ int newContact()
 
 	} while (validate != 0);
 
+	strcpy(contact[counter].name, name); // save name to contact[index].name
+	strcpy(contact[counter].phoneNumber, phoneNumber); // save phone number to contact[index].phoneNumber 
+
 	puts("");
 	printf("New contact successfully added!\n");
+	printf("%s\n%s\n", contact[counter].name, contact[counter].phoneNumber);
 
 	system("pause");
 	return 0;
 }
 
 /* List contact from %Structure_Variables */
-int listContact()
+int listContact(int, struct phoneBook*)
 {
+
+	/* ASCII table
+	65	A
+	90	Z
+	97	a
+	122	z
+	*/
+
 	int i;
+
 
 	system("cls");
 	printf("View List Contact\n");
 	puts("");
+
+	printf("%s %3s %-30s %12s\n", "", "#", "Name", "Phone Number"); // header
+
+	printf("%s %3s %-30s %12s\n", "", "#", "Name", "Phone Number"); // body
 
 	puts("");
 	system("pause");
@@ -138,10 +172,12 @@ int listContact()
 }
 
 /* Search contact from %Structure_Variables */
-int searchContact()
+int searchContact(int, struct phoneBook*)
 {
 	int i,
 		found = 0;
+
+	struct phoneBook rekontruksi[200]; // recountruction
 
 	system("cls");
 	printf("Search Contact\n");
@@ -153,7 +189,7 @@ int searchContact()
 }
 
 /* Delete contact from %Structure_Variables */
-int deleteContact()
+int deleteContact(int, struct phoneBook*)
 {
 
 	system("cls");
@@ -164,8 +200,36 @@ int deleteContact()
 	return 0;
 }
 
+/* Reconstruct contact to %Structure_Variables */
+int sortContact(int counter, struct phoneBook *contact)
+{
+
+	/* ASCII table
+	65	A
+	90	Z
+	97	a
+	122	z
+	*/
+	int i;
+	struct phoneBook contactBackup[200]; // recountruction
+
+	// backup contact data
+	for (i = 0; i < counter; i++)
+	{
+		contactBackup[i] = contact[i];
+	}
+	
+
+	for (i = 0; i < counter; i++)
+	{
+
+	}
+
+	return 0;
+}
+
 /* Store contact from %Structure_Variables into Contact.txt*/
-int storeContact()
+int storeContact(int, struct phoneBook*)
 {
 
 	return 0;
@@ -173,8 +237,14 @@ int storeContact()
 
 
 /*
-reference:
+
+known bug:
 
 - fflush(stdin) issue in Microsoft Visual Studio 2015 @ http://stackoverflow.com/a/33216224
+- Debug Assertion Failed @ http://bit.ly/1IwaBMC
+	Debug Assertion Failed!; File: minkernel\crts\ucrt\src\appcrt\convert\isctyple.cpp; Line: 36; Expression: c >= -1 && c <=255
+  when blank input in 
+	scanf("%[^\n]", name);
+	scanf("%[^\n]", phoneNumber);
 
 */
